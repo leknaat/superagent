e7d6f487c855e11c9c9e6290164fe436e12fa360
leknaat <wishfire@gmail.com>
2017-03-15 15:20:38 +0000

    All done in html index for simplicity. Check console, the object is being returned :)

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Router, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
+import Request from 'superagent'
 
 import routes from '../../routes'
 import createStore from '../../redux/store'
